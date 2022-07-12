ae3c7b51de898de20b7dbb18e5612399c231b3f5
Wuuzzaa <jonas-licht@gmx.de>
2022-07-12 22:57:04 +0200

    all datasets can be downloaded, imputed and ohe

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from pathlib import Path
 import openml
 import pandas as pd
 
-# load study
 from feature_engine.encoding import OneHotEncoder
+from feature_engine.imputation import RandomSampleImputer, CategoricalImputer
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
+random_state = 42
+
+# load study
 suite = openml.study.get_suite(99)
 print(suite)
 
@@ -19,6 +22,10 @@ for task_id in tasks:
     print()
     task = openml.tasks.get_task(task_id)
     print(task)
+
+    # show dataset id is not equal the taskid
+    print()
+    print(f"dataset_id: {str(task.dataset_id)}")
 
     # check if already done -> skip the task/dataset
     path = Path(f".//data//{str(task.dataset_id)}//")
@@ -36,18 +43,27 @@ for task_id in tasks:
     target = dataset.default_target_attribute
     X, y, categorical_indicator, attribute_names = dataset.get_data(target=target)
 
+    # impute NaN values
+    X = CategoricalImputer(ignore_format=True).fit_transform(X, y)
+
     # X one hot encode
     try:
-        X = OneHotEncoder().fit_transform(X, y)
-    except ValueError:
-        warnings.warn("ValueError: No categorical variables found in this dataframe. -> No One hot encoding used.")
+        encoder = OneHotEncoder()
+        X = encoder.fit_transform(X, y)
+    except ValueError as e:
+        # not nice but there are different value error thrown so we need to handle by error message
+        if "No categorical variables found in this dataframe" in str(e):
+            warnings.warn("ValueError: No categorical variables found in this dataframe. -> No One hot encoding used.")
+        else:
+            raise
 
+    # store feature names
     feature_names_encoded = X.columns
 
     # Standardscale X
     X = StandardScaler().fit_transform(X)
 
-    # get a dataframe again after scaling
+    # get a dataframe again after scaling from ndarray
     X = pd.DataFrame(X)
     X.columns = feature_names_encoded
 
@@ -58,14 +74,12 @@ for task_id in tasks:
     X["y"] = y
     X = X.sample(frac=1).reset_index(drop=True)
 
-    # split y from X again
+    # split y and X again
     y = X["y"]
     X = X.drop(labels="y", axis="columns")
 
     # create folder for dataset
-    if not path.exists():
-        print(f"create folder {path}")
-        path.mkdir(parents=True, exist_ok=True)
+    path.mkdir(parents=True, exist_ok=True)
 
     # store X
     X.to_feather(path=path_X)
